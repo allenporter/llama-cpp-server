43187f29b5bced68823fb7427ec5f583d81b4950
Allen Porter <allen.porter@gmail.com>
2023-12-12 00:15:19 -0800

    Add --n_gpu_layers

--- server/start.sh
+++ server/start.sh
@@ -3,6 +3,8 @@
 set -e
 export
 
+: ${N_GPU_LAYERS:=0}
+
 if [ -z "${MODEL_DIR}" ]; then
   echo "Required env MODEL_DIR not set"
   exit 1
@@ -19,5 +21,5 @@ if [ ! -f "${MODEL_FILE}" ]; then
     exit 1
 fi
 
-echo "Running model server with model ${MODEL_FILE}"
-python3 -m llama_cpp.server --model ${MODEL_FILE}
+echo "Running model server with model ${MODEL_FILE} and ${N_GPU_LAYERS} GPU layers"
+python3 -m llama_cpp.server --model ${MODEL_FILE} --n_gpu_layers=${N_GPU_LAYERS}
